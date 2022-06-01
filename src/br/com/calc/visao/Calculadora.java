bc19faf060f01ca473107b7f3022dac22ee1f84e
Wedney Almeida <wedneypaixao.negocios@gmail.com>
2022-06-01 18:03:22 -0300

    Criate classe calculator

--- src/br/com/calc/visao/Calculadora.java
+++ src/br/com/calc/visao/Calculadora.java
@@ -8,5 +8,12 @@ public class Calculadora extends JFrame {
     public Calculadora(){
 
         setSize(232, 322);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Calculadora();
     }
 }
