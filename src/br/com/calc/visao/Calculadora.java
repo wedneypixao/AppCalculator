b6f4d996857da5207b95c407fe5603e9bfdbcde8
Wedney Almeida <wedneypaixao.negocios@gmail.com>
2022-06-01 17:59:34 -0300

    Initial commit

---
+++ src/br/com/calc/visao/Calculadora.java
@@ -0,0 +1,12 @@
+package br.com.calc.visao;
+
+import javax.swing.JFrame;
+
+@SuppressWarnings("serial")
+public class Calculadora extends JFrame {
+
+    public Calculadora(){
+
+        setSize(232, 322);
+    }
+}
