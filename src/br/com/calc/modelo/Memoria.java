22257f89208453998d2c160fb221819e0bcb6dbe
Wedney Almeida <wedneypaixao.negocios@gmail.com>
2022-06-04 07:42:40 -0300

    Creating a method to test the type of data entered in the calculator

--- src/br/com/calc/modelo/Memoria.java
+++ src/br/com/calc/modelo/Memoria.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 public class Memoria {
 
+    private enum TipoComando {
+        ZERAR, NUMERO, DIV, MULT, SUB, SOMA, IGUAL, VIRGULA;
+    };
     private static final Memoria instancia = new Memoria();
 
     private  final List<MemoriaObservador> observadores =
@@ -28,12 +31,45 @@ public class Memoria {
         return textoAtual.isEmpty() ? "0" : textoAtual;
     }
 
-    public void processarComando(String valor) {
-        if("AC".equals(valor)){
+    public void processarComando(String texto) {
+
+        TipoComando tipoComando = detectarTipoComando(texto);
+
+        if("AC".equals(texto)){
             textoAtual = "";
         } else {
-            textoAtual += valor;
+            textoAtual += texto;
         }
         observadores.forEach(o -> o.valorAlterado(getTextoAtual()));
+    }
+
+    private TipoComando detectarTipoComando(String texto) {
+        if(textoAtual.isEmpty() && texto == "0"){
+           return null;
+        }
+
+        try {
+            Integer.parseInt(texto);
+            return TipoComando.NUMERO;
+        } catch (NumberFormatException e){
+            // Quando não for número...
+            if ("AC".equals(texto)){
+                return TipoComando.ZERAR;
+            } else if ("/".equals(texto)) {
+                return TipoComando.DIV;
+            } else if ("*".equals(texto)) {
+                return TipoComando.MULT;
+            } else if ("+".equals(texto)) {
+                return TipoComando.SOMA;
+            } else if ("-".equals(texto)) {
+                return TipoComando.SUB;
+            } else if ("=".equals(texto)) {
+                return TipoComando.IGUAL;
+            } else if (",".equals(texto)) {
+                return TipoComando.VIRGULA;
+            }
+        }
+
+        return null;
     }
 }
